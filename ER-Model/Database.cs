43f7de3a2350a4d14235ae086442874de5fd251f
Rene Ederer <rene.ederer.nbg@gmail.com>
2019-01-20 17:40:09 +0100

    Initial commit

---
+++ ER-Model/Database.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ER_Model
+{
+    public class Database
+    {
+        OracleConnection Connection { get; set; }
+        public Database()
+        {
+            Connection = new OracleConnection();
+            Connection.ConnectionString = "User Id=system;Password=1234;Data Source=127.0.0.1:1521/ORA12C";
+            Connection.Open();
+        }
+
+        public ERModel GetERModel(
+            string owner,
+            string tableName,
+            ERModel erModel,
+            int depth)
+        {
+            using (var command = Connection.CreateCommand())
+            {
+                command.CommandText = $@"SELECT
+  columnMetaData.owner columnMetaData_owner,
+  columnMetaData.table_name columnMetaData_table_name,
+  columnMetaData.column_name columnMetaData_column_name,
+  columnMetaData.nullable,
+  refData.referencedOwner,
+  refData.referencedTableName,
+  refData.referencedColumnName
+from
+    all_tab_columns columnMetaData
+left join
+    (select
+        referencingTable.owner as referencingOwner,
+        referencingTable.table_name as referencingTableName,
+        referencingTable.column_name as referencingColumnName,
+        referencedTable.owner as referencedOwner,
+        referencedTable.table_name as referencedTableName,
+        referencedTable.column_name as referencedColumnName,
+	referencingTable.column_name
+     from all_cons_columns referencingTable
+     join all_cons_columns referencedTable on referencingTable.position = referencedTable.position
+     join all_constraints
+         on referencingTable.owner = all_constraints.owner
+        and referencedTable.constraint_name = all_constraints.r_constraint_name
+        and referencedTable.owner = all_constraints.r_owner
+        and referencingTable.constraint_name = all_constraints.constraint_name
+        and all_constraints.constraint_type = 'R') refData
+    on columnMetaData.owner = referencingOwner
+       and columnMetaData.table_name = referencingTableName
+       and columnMetaData.column_name = referencingColumnName
+
+where
+
+      columnMetaData.owner = upper('{owner}')
+  and columnMetaData.table_name = upper('{tableName}')";
+                List<ReferenceColumn> toReferences = new List<ReferenceColumn>();
+                List<Column> normalColumns = new List<Column>();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.IsDBNull(4))
+                        {
+                            normalColumns.Add(
+                                new Column
+                                {
+                                    ColumnName = reader.GetString(2),
+                                    ColumnNullable = reader.GetString(3) == "Y",
+                                    ColumnType = ""
+                                });
+                        }
+                        else
+                        {
+                            toReferences.Add(
+                                new ReferenceColumn
+                                {
+                                    ColumnName = reader.GetString(2),
+                                    ColumnNullable = reader.GetString(3) == "Y",
+                                    ReferenceOwnerName = reader.GetString(4),
+                                    ReferenceTableName = reader.GetString(5),
+                                    ReferenceColumnName =  reader.GetString(6)
+                                });
+                        }
+                    }
+                }
+                Table table =
+                    new Table
+                    {
+                        Owner = owner.ToUpper(),
+                        Name = tableName.ToUpper(),
+                    };
+
+                if(!erModel.Tables.ContainsKey(table))
+                {
+                    erModel.Tables.Add(
+                        table,
+                        new TableColumns
+                        {
+                            To = toReferences,
+                            NormalColumns = normalColumns
+                        });
+                }
+                if (depth > 0)
+                {
+                    foreach (var t in toReferences)
+                    {
+                        GetERModel(t.ReferenceOwnerName, t.ReferenceTableName, erModel, depth - 1);
+                    }
+                }
+                return erModel;
+            }
+        }
+    }
+}
---
+++ ER-Model/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ER_Model
+{
+
+    [Serializable]
+    public class ERModel
+    {
+        public Dictionary<Table, TableColumns> Tables { get; set; } = new Dictionary<Table, TableColumns>();
+    }
+
+    [Serializable]
+    public struct Table
+    {
+        public string Owner { get; set; }
+        public string Name { get; set; }
+    }
+
+    [Serializable]
+    public class Column
+    {
+        public string ColumnName { get; set; } = "";
+        public bool ColumnNullable { get; set; } = false;
+        public string ColumnType { get; set; } = "";
+    }
+
+    [Serializable]
+    public class ReferenceColumn : Column
+    {
+        public string ReferenceOwnerName { get; set; }
+        public string ReferenceTableName { get; set; }
+        public string ReferenceColumnName { get; set; }
+    }
+
+    [Serializable]
+    public class TableColumns
+    {
+
+        public IEnumerable<ReferenceColumn> From { get; set; } = new List<ReferenceColumn>();
+        public IEnumerable<ReferenceColumn> To { get; set; } = new List<ReferenceColumn>();
+        public IEnumerable<Column> NormalColumns { get; set; } = new List<Column>();
+    }
+    public partial class MainWindow : Window
+    {
+        ERModel erModel = new Database().GetERModel("entwickler", "bewmail", new ERModel(), 0);
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            addERModel(erModel);
+            WriteToBinaryFile("c:/users/rene/myFile.dat", erModel);
+            var x = ReadFromBinaryFile<ERModel>("c:/users/rene/myFile.dat");
+   //         addERModel(x);
+        }
+        public static void WriteToBinaryFile<T>(string filePath, T objectToWrite)
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Create))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, objectToWrite);
+            }
+        }
+        public static T ReadFromBinaryFile<T>(string filePath)
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(stream);
+            }
+        }
+
+        public void addERModel(ERModel erModel)
+        {
+            TreeView t = new TreeView();
+            var item = getCanvasERModel(erModel, "ENTWICKLER", "BEWMAIL", null);
+            t.Items.Add(item);
+            Canvas.SetLeft(t, 10);
+            Canvas.SetTop(t, 20);
+            grid.Children.Add(t);
+            item.Selected += Item_Selected;
+        }
+
+        private void Item_Selected(object sender, RoutedEventArgs e)
+        {
+            var t = sender as TreeViewItem;
+            MessageBox.Show("Clicked: " + t.Header);
+        }
+
+
+
+        public TreeViewItem getCanvasERModel(ERModel erModel, string owner, string tableName, string header, bool isRecursive = false)
+        {
+            var t = new TreeViewItem();
+            t.Header = header ?? (owner + "." + tableName);
+            if(!erModel.Tables.ContainsKey(new Table { Owner = owner, Name = tableName }) || isRecursive)
+            {
+                t.Expanded += T_Expanded;
+                t.Items.Add(owner);
+                t.Items.Add(tableName);
+            }
+            else
+            {
+                foreach(var column in erModel.Tables[new Table { Owner = owner, Name = tableName }].NormalColumns)
+                {
+                    t.Items.Add(column.ColumnName + " (" + (column.ColumnNullable ? "null" : "not null") + ")");
+                }
+                foreach (var r in erModel.Tables[new Table { Owner = owner, Name = tableName }].To)
+                {
+                    t.Items.Add(
+                        getCanvasERModel(
+                            erModel,
+                            r.ReferenceOwnerName,
+                            r.ReferenceTableName,
+                            r.ColumnName + "(" +
+                              (r.ColumnNullable ? "null" : "not null") +
+                              ") => " + r.ReferenceTableName,
+                            (owner == r.ReferenceOwnerName && tableName == r.ReferenceTableName)
+                        )
+                    );
+                }
+            }
+            return t;
+        }
+
+        private void T_Expanded(object sender, RoutedEventArgs e)
+        {
+            var treeViewItem = sender as TreeViewItem;
+            treeViewItem.Expanded -= T_Expanded;
+            var owner = treeViewItem.Items[0].ToString();
+            var tableName = treeViewItem.Items[1].ToString();
+            treeViewItem.Items.Clear();
+            erModel = new Database().GetERModel(owner, tableName, erModel, 1);
+            foreach(var column in erModel.Tables[new Table { Owner = owner, Name = tableName }].NormalColumns)
+            {
+                treeViewItem.Items.Add(column.ColumnName + " (" + (column.ColumnNullable ? "null" : "not null") + ")");
+            }
+            foreach (var r in erModel.Tables[new Table { Owner = owner, Name = tableName }].To)
+            {
+                treeViewItem.Items.Add(
+                    getCanvasERModel(
+                        erModel,
+                        r.ReferenceOwnerName,
+                        r.ReferenceTableName,
+                        r.ColumnName + "(" +
+                          (r.ColumnNullable ? "null" : "not null") +
+                          ") => " + r.ReferenceTableName));
+            }
+        }
+    }
+}
